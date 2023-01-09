7afeceddf0b38d7bc36ab9ff80fa137d49d86f5e
Branko Záchemský <bzachemsky@gmail.com>
2023-01-09 08:54:35 +0100

    dcy

---
+++ DadaZap/cmake-build-debug/CMakeFiles/DadaZap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DadaZap.dir/main.c.o"
+  "DadaZap"
+  "DadaZap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DadaZap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
